5c4652a4ceb8f5cdc559c5652dcb074c4d87fef8
GameDevRocky <159648130+GameDevRocky@users.noreply.github.com>
2025-02-22 14:50:12 -0500

    gg

--- afro-pix/src/pages/home.jsx
+++ afro-pix/src/pages/home.jsx
@@ -7,15 +7,13 @@ import Businesses from "../components/Businesses.jsx"
 export function Home(){
     return <div className="flex flex-col">
         <NavBar/>
+        <SearchBar/>
         <div className="w-full flex h-96 p-4 gap-4">
             <div className="rounded-md overflow-clip w-1/2 h-full">
-
             <Map/>
             </div>
             <div className="w-1/2 flex flex-col h-full">
-                <div className="w-full h-full flex-col justify-center items-center py-8 bg-amber-700 flex shadow-lg shadow-neutral-400 rounded-md">
-                <SearchBar/>
-
+                <div className="w-full h-full justify-center items-center py-8 bg-amber-700 flex shadow-lg shadow-neutral-400 rounded-md">
                     <Businesses/>
                 </div>
             </div>
